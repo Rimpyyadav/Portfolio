157838f046654763af9eefa6f22cd041b36a6a6a
Rimpy <yadavrimpy1624@gmail.com>
2024-08-15 22:24:09 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Tech from './components/Tech'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       <About/>
       <Tech/>
       <Projects/>
+      <Contact/>
     </div>
     </div>
   )
