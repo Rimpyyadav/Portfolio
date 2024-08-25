d18200b22b7204f3b23a5a178eeae2f34313feca
Rimpy <yadavrimpy1624@gmail.com>
2024-08-26 02:18:09 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
+import aboutpic from "../assets/about.jpg"
 
 function About() {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text 4xl">
+      <h1 className="my-20 text-center text-4xl">
         About
         <span className="text-neutral-500"> Me</span>
-      </h2>
+      </h1>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+         whileInView={{opacity:1, x:0}}
+         initial={{opacity: 0, x:-100}}
+         transition={{duration:0.5}}
+        className="w-full lg:w-1/2 lg:p-8">
           <div className="flex items-center justfy-center"></div>
-        </div>
+          <img src={aboutpic} style={{ height: '500px' , width: '500px' }}  alt="" />
+        </motion.div>
         <motion.div
         whileInView={{opacity:1, x:0}}
         initial={{opacity: 0, x:100}}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import {HERO_CONTENT} from "../constants"
 import { motion } from "framer-motion"
+import profilpic from "../assets/profilepic.jpg"
 
 const container = (delay) => ({
     hidden: {x: -100, opacity: 0},
@@ -39,7 +40,13 @@ const Hero = () => {
                 </div>
                 </div> 
                 <div className="w-full lg:w-1/2 lg:p-8" >
-                <div className="flex justify-center" ></div>
+                <div className="flex justify-center" >
+                <motion.img 
+                initial={{x:100, opacity:0}}
+                animate={{x:0, opacity:1}}
+                transition={{duration:1, delay:1.2}}
+                src={profilpic} style={{ height: '500px' , width: '350px' }}  alt="Profile Pic" />
+                </div>
                 </div>
             </div>
         </div>
