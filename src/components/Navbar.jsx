3928e745674499e4a75f08800e95aca90e0801d4
Rimpy <yadavrimpy1624@gmail.com>
2024-08-28 23:59:43 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
 import logo from "../assets/logo.jpg";
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaTwitter} from "react-icons/fa";
-
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-  <nav className=' mb-20 flex items-center justify-between py-6' >
-    <div className="flex flex-shrink-0 items-center" >
-        <img className='mx-5 w-20 rounded-full' src= {logo} alt="logo" ></img>
-    </div>
-    <div className = "m-8 flex items-center justify-between gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaTwitter/>
-    </div>
-  </nav>
-  )
+    <nav className='mb-20 flex items-center justify-between py-6'>
+      <div className="flex flex-shrink-0 items-center">
+        <img className='mx-5 w-20 rounded-full' src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-between gap-4 text-2xl">
+        <div className="flex space-x-4">
+          <a href="#home" className="text-white hover:text-gray-300">Home</a>
+          <a href="#about" className="text-white hover:text-gray-300">About</a>
+          <a href="#skills" className="text-white hover:text-gray-300">Skills</a>
+          <a href="#projects" className="text-white hover:text-gray-300">Projects</a>
+          <a href="#contact" className="text-white hover:text-gray-300">Contact</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
